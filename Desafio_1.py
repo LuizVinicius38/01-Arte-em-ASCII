3afdeba8f9607f49c2559e028acac2f607d3a449
LuizVinicius38 <89944844+LuizVinicius38@users.noreply.github.com>
2021-09-08 19:30:09 -0300

    Add files via upload

---
+++ Desafio_1.py
@@ -0,0 +1,2 @@
+print("O que você está pensando?")
+print("estou pensando: Se vou ser um ótimo programador")
---
+++ Desafio_3.py
@@ -0,0 +1,4 @@
+print("ha " *4)
+print("ba" + "na" *2)
+e = "Bra" + "sil" "!"
+print(e * 10)
